e1a0baad096b3bb2ad1cedaeffc445a688819c3f
Samuel <mucasantos@gmail.com>
2023-01-06 15:48:41 -0300

    create pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container  mx-auto px-3 pb-12">content</main>
+        <main className="container  mx-auto px-3 pb-12">
+<Routes>
+  <Route path="/" element={<Home />}></Route>
+  <Route path="/about" element={<About />}></Route>
+  <Route path="/notfound" element={<NotFound />}></Route>
+  <Route path="/*" element={<NotFound />}></Route>
+</Routes>
+
+        </main>
         <Footer/>
       </div>
   
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <div>
+            <h1 className="text-6xl">Welcome!</h1>
+        </div>
+    )
+}
+
+export default About
