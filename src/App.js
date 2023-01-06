d85276297af905ede57f7a2f2272906b44ed7b70
Samuel Luis dos Santos <mucasantos@gmail.com>
2023-01-06 15:24:13 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container  mx-auto px-3 pb-12">content</main>
+        <Footer/>
+      </div>
+  
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  const footerYear = new Date().getFullYear();
+
+  return (
+    <footer
+      className="footer p-10 bg-gray-700
+    text-primary-content footer-center"
+    >
+      <div>
+        <p>Copyright &copy; {footerYear} All reserved</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
